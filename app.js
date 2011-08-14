35c3e8a669ef6eb2bd39e80f6142884d91fe2a33
David Hong <davidhong.code@gmail.com>
2011-08-14 23:17:12 +1000

    port changed

--- app.js
+++ app.js
@@ -39,5 +39,9 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// Start the engine
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
